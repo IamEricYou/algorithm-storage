98857b705b32b867fd006098dd29d5d34b7a3247
Jaehyung You (Eric) <bapsajura@naver.com>
2020-02-21 17:51:20 +0900

    Update inventory_allocator.py

--- ETC/inventory_allocator.py
+++ ETC/inventory_allocator.py
@@ -2,20 +2,29 @@ class  InventoryAllocator():
     def __init__(self, order, inventory):
         self.order = order
         self.inventory = inventory
+        order_list = []
 
     def allocation(self):
         # if not order or not self.inventory:
         #     return []
-        print(order,type(inventory))
-        for key in inventory[1].keys():
-            print(inventory[1].get(key))
-        # for key in order.keys():
-        #     self.calculate_inventory(key)
+        print(order,inventory)
+        # for item in inventory:
+        #     self.parse_inventory(item)
+        for key in order.keys():
+            #self.calculate_inventory(key)
+            if(self.check_inventory_have_item(key)):
+                return []
 
-    def calculate_inventory(self, key):
-        for item in inventory.keys():
-            print(item)
+    def parse_inventory(self, item):
+        for x in item['inventory']:
+            self.calculate_inventory(x)
 
+    def calculate_inventory(self, product):
+        for x in order[product]:
+            print(x)
+    
+    def check_inventory_have_item(self, item):
+        print(item)
 
 
 if __name__ == '__main__':
