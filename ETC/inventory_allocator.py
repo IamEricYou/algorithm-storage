6b04727216fc7bba22f33e3643bae97a49e575c1
Jaehyung You (Eric) <bapsajura@naver.com>
2020-02-23 12:44:18 +0900

    Update inventory_allocator.py

--- ETC/inventory_allocator.py
+++ ETC/inventory_allocator.py
@@ -1,30 +1,40 @@
-class  InventoryAllocator():
+class InventoryAllocator():
     def __init__(self, order, inventory):
         self.order = order
         self.inventory = inventory
         order_list = []
 
     def allocation(self):
+        total_inventory = {}
         # if not order or not self.inventory:
         #     return []
-        print(order,inventory)
+        print(order, inventory)
         # for item in inventory:
         #     self.parse_inventory(item)
-        for key in order.keys():
-            #self.calculate_inventory(key)
-            if(self.check_inventory_have_item(key)):
-                return []
+        for x in inventory:
+            self.parse_inventory(x)
+            
+            
+            # self.calculate_inventory(key)
+            # if(not(self.check_inventory_have_item(key))):
+            #     return []
 
     def parse_inventory(self, item):
-        for x in item['inventory']:
-            self.calculate_inventory(x)
+        print(item)
 
     def calculate_inventory(self, product):
         for x in order[product]:
             print(x)
-    
+
+    #Call
     def check_inventory_have_item(self, item):
-        print(item)
+        for inventory_dict in inventory:
+            for item_in_inventory in inventory_dict['inventory']:
+                if item_in_inventory != item:
+                    print(item_in_inventory)
+                    print(item)
+                    print("Inventory doesn't have what you order")
+                    return False
 
 
 if __name__ == '__main__':
@@ -34,10 +44,9 @@ if __name__ == '__main__':
     # key, value = var.split()
     # d[key] = int(value)
 
-    order = {'melon': 1, 'banana': 2, 'orange': 9}
-    inventory = [{'name': 'owd', 'inventory': {'apple': 1, 'orange': 10}}, {'name': 'dm', 'inventory': {'banana': 5, 'orange': 5} }]
-    ia = InventoryAllocator(order,inventory)
+    order = {'banana': 2, 'orange': 9}
+    inventory = [{'name': 'owd', 'inventory': {'apple': 1, 'orange': 10}}, {
+        'name': 'dm', 'inventory': {'banana': 5, 'orange': 5}}]
+    ia = InventoryAllocator(order, inventory)
 
-    ia.allocation()
-
-   
+    print(ia.allocation())
