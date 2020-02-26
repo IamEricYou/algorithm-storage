98ae2faba2e9337ab8b26408578b70f697c9410d
Jaehyung You (Eric) <bapsajura@naver.com>
2020-02-26 18:12:29 +0900

    Inventory allocator finished

--- ETC/inventory_allocator.py
+++ ETC/inventory_allocator.py
@@ -6,18 +6,21 @@ class InventoryAllocator():
         self.inventory = inventory
         self.return_dict = {}
 
+    #Main function to run
     def run_allocation(self):
-        print(order, inventory)
         total_inventory = self.merge_inventories()
-        if self.check_inventory_have_item(total_inventory):
-            self.is_order_valid = False;
-            print("Here")
+        if not self.check_inventory_have_item(total_inventory):
             return []
-            
-        #print(total_inventory)
-        output_inventory = self.process_order(total_inventory)
-        print(self.return_dict)
 
+        self.process_order(total_inventory)
+        
+        return_list = []
+        for x in self.return_dict:
+            return_list.append({x: self.return_dict[x]})
+
+        return return_list
+
+    #Merge dictionaries (map) into one
     def merge_inventories(self):
         temp_inventory = {}
         swap = {}
@@ -30,32 +33,29 @@ class InventoryAllocator():
 
         return dict(swap)
 
+    #Parse dictionaries to be processed
     def process_order(self, inv):
-        temp_inventory = {}
         for item in inv:
             for x in self.inventory:
                 if item in x['inventory']:
-                    swap_inv = self.calculate_order(item, x)
-                    if swap_inv is not None:
-                        temp_inventory = {**temp_inventory, **swap_inv}
+                    self.calculate_order(item, x)
 
-        return temp_inventory
 
+    #Calculate the order
     def calculate_order(self, item, each_warehouse):
-        #{x.get('name'): {item: (self.order.get(item) - x['inventory'].get(item))} }
         if(self.order.get(item) == 0):
             return
             
-        temp_value = each_warehouse['inventory'].get(item) - self.order.get(item) #4
+        temp_value = each_warehouse['inventory'].get(item) - self.order.get(item)
         swap_value = self.order.get(item)
-        print("Return",self.return_dict)
+
         if(temp_value <= 0):
             self.order[item] = self.order.get(item) - each_warehouse['inventory'].get(item)
             if each_warehouse['name'] in self.return_dict:
                 if item in self.return_dict[each_warehouse['name']]:
                     self.return_dict[each_warehouse['name']] = {item : (self.return_dict[each_warehouse['inventory']].get(item) + each_warehouse['inventory'].get(item))}
                 else:
-                    self.return_dict[each_warehouse['name']] = {item : each_warehouse['inventory'].get(item) }
+                    self.return_dict[each_warehouse['name']][item] = each_warehouse['inventory'].get(item)
             else:
                 self.return_dict[each_warehouse['name']] = {item : each_warehouse['inventory'].get(item) }
         else:
@@ -64,26 +64,28 @@ class InventoryAllocator():
                 if item in self.return_dict[each_warehouse['name']]:
                     self.return_dict[each_warehouse['name']] = {item : self.return_dict[each_warehouse['inventory']].get(item) + temp_value}
                 else:
-                    self.return_dict[each_warehouse['name']] = {item : swap_value}
+                    self.return_dict[each_warehouse['name']][item] = swap_value
             else:
                 self.return_dict[each_warehouse['name']] = {item : swap_value}
        
 
-    #Call
+    #Check the order is valid to be made
     def check_inventory_have_item(self, inv):
         for order_item in self.order:
             if order_item not in inv:
-                print("Inventory doesn't have the item in the order")
+                #print("Inventory doesn't have the item in the order")
                 return False
             
             if self.order[order_item] > inv[order_item]:
-                print("Inventory doesn't have enough item to order.")
+                #print("Inventory doesn't have enough item to order.")
                 return False
+        return True
 
 if __name__ == '__main__':
     order = {'banana': 5, 'orange': 10}
     inventory = [{'name': 'owd', 'inventory': {'banana': 1, 'orange': 10}}, {
         'name': 'dm', 'inventory': {'banana': 5, 'orange': 5}}]
     ia = InventoryAllocator(order, inventory)
-    
+
+    #Print this line to test
     ia.run_allocation()
