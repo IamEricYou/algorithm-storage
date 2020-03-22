f8fffbd2c6d6b658807eff44e33a38aa8d6960db
Jaehyung You (Eric) <bapsajura@naver.com>
2020-03-22 18:37:22 +0900

    Create camo.py

---
+++ P/camo.py
@@ -0,0 +1,14 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42578
+
+from collections import Counter
+
+
+def solution(clothes):
+
+    counter_each_category = Counter([cat for _, cat in clothes])
+    all_possible = 1
+    for key in counter_each_category:
+        all_possible *= (counter_each_category[key] + 1)
+        return all_possible - 1
+
+
