4c90d334727dfd415b258fcf10bbd993ad7dde3c
Jaehyung You (Eric) <bapsajura@naver.com>
2020-04-26 22:29:11 +0900

    Update dp.py

--- P/dp.py
+++ P/dp.py
@@ -28,3 +28,19 @@ number = 12 # 4
 # number = 11 # 3
 
 solution(N, number)
+
+# 1463
+# https://lee-seul.github.io/algorithm/2017/03/16/dynamic-programming.html
+x = int(input())
+
+dp = [0 for _ in range(x+1)]
+dp[1] = 0
+
+for i in range(2, x+1):
+    dp[i] = dp[i-1] + 1
+    if not i % 2 and dp[i] > dp[i//2] + 1:
+        dp[i] = dp[i//2] + 1
+    if not i % 3 and dp[i] > dp[i//3] + 1:
+        dp[i] = dp[i//3] + 1
+
+print(dp[x])
