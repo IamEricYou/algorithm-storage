fbaa096f5918f78e7112e000e26945bb9dbd9629
IamEricYou <bapsajura@naver.com>
2020-06-15 16:46:05 +0900

    UPDATE: some more location migration logic

--- template/migration_template.py
+++ template/migration_template.py
@@ -1,6 +1,19 @@
 import json
 
-with open('prod_temp.json') as f:
+with open('prod_temp1.json') as f:
     data = json.load(f)
 
-print(data)
+data_prod_desc = data.get('prod').get('description_module')
+experience_location = data_prod_desc.get('PMDL_EXPERIENCE_LOCATION')
+experience_location = experience_location.get('content').get('list')
+
+for item in experience_location:
+    print("EXP LO: ", item.get('latlng'))
+
+venue_location = data_prod_desc.get('PMDL_VENUE_LOCATION')
+location_type = venue_location.get('module_title')
+venue_location = venue_location.get('content').get('list')
+
+print(location_type)
+for item in venue_location:
+    print("VENUE LO: ", item.get('latlng'))
