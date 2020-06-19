b962d5994f056787b190e67654bf97ef1c1fd279
IamEricYou <bapsajura@naver.com>
2020-06-19 09:24:33 +0900

    UPDATE: update some xlsx parser

--- template/xlsx_parser.py
+++ template/xlsx_parser.py
@@ -8,7 +8,17 @@ def replace_nan_to_empty(column_list):
 
 def get_rows():
     file = pd.read_excel('template/02.99 Categorization.xlsx')
-    print(file.columns)
+    
+    prod_name_list = replace_nan_to_empty(file[1])
+    prod_name_list = list(filter(('empty').__ne__, prod_name_list)) #remove empty
+
+
+    file_list = file.values.T.tolist() # this can make df to list
+    # file_list_remove_nan = [[i for i in x if pd.notnull(i)] for x in file_list]
+    # print(file_list[4])
+
+    # 일단 나중에 태그작업하게되면 일일히 행 번호 지정해서 하는게 제일 빠를듯 - 왜냐면 행 태그이름이 바뀔수도있으니
+
     # file.rows doens't work
 
 if __name__ == "__main__":
