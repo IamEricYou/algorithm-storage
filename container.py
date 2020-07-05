0a09033b91d3724e320f1abd9785398e35f9c817
Jaehyung You <jaehyung.you@travelflan.com>
2020-07-05 17:12:14 +0900

    UPDATE: some algorithm

--- container.py
+++ container.py
@@ -11,14 +11,23 @@ adjustment = {
     'opera': 'carmen'
 }
 
-c = ChainMap(adjustment, baseline)
+# c = ChainMap(adjustment, baseline)
+#
+# d = c.new_child()
+# e = c.new_child()
+#
+#
+# cnt = Counter()
+# for word in ['red', 'blue', 'red', 'green', 'blue', 'blue']:
+#     cnt[word] += 1
+#
+# print(cnt)
 
-d = c.new_child()
-e = c.new_child()
+
+def most_frequent_count(li):
+    return max(set(li), key=li.count)
 
 
-cnt = Counter()
-for word in ['red', 'blue', 'red', 'green', 'blue', 'blue']:
-    cnt[word] += 1
-
-print(cnt)
+if __name__ == '__main__':
+    temp = [10, 20, 10, 40, 40, 40, 50]
+    print(most_frequent_count(temp))
