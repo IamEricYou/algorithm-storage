efcfe8e99d261be0f76dc5b9effbd32b586cb7a2
Jaehyung You <jaehyung.you@travelflan.com>
2020-07-19 22:44:54 +0900

    ADD: Some parse xlsx parsing file

--- parsse.py
+++ parsse.py
@@ -2,16 +2,45 @@ import csv
 import pandas as pd
 import numpy as np
 
-attr_data = pd.read_excel('data_summary.xlsx', sheet_name='Attr_data')
+def check_value_is_nan(val):
+    if type(val) == float and np.isnan(val):
+        return True
+    return False
 
-category = attr_data['구분']
-city = attr_data['City']
-area = attr_data['Area']
-area_list = []
+file = pd.ExcelFile('02.99 Categorization.xlsx')
 
-for cate,ci,ar, in zip(category,city,area):
-    if cate == 'TravelFlan' and ci == '타이베이':
-        if ar not in area_list:
-            area_list.append(ar)
-            
-print(area_list)
+recommended_itinerary_sheet_name = '추천일정_Template'
+max_tag_num = sum(x.startswith('item_') for x in pd.read_excel('02.99 Categorization.xlsx', sheet_name=recommended_itinerary_sheet_name).columns.tolist()) # check how many tags in column name
+rcm_itnry_sheet = file.parse(recommended_itinerary_sheet_name)
+
+for idx, each_row in rcm_itnry_sheet.iterrows():
+    how_many_days = 1
+
+    if each_row['id'] == "ex" or check_value_is_nan(each_row['id']):
+        continue
+
+    if each_row['day_index'] == 1:
+        tendency_codes = each_row['tendency_code'].split(',')
+        tendency_degrees = {
+            'activity': int(each_row[5]),
+            "city_nature": int(each_row[6]),
+            "willingness": int(each_row[7])
+        }
+
+        ri_template = {
+            'title': each_row['title'],
+            'status': 'DCT',
+            'description': each_row['desc'],
+            'tendency_codes': tendency_codes,
+            'tendency_degrees': tendency_degrees,
+        }
+
+        # obj, _ = RecommendedItinerary.objects.update_or_create(
+        #     id=int(each_row['id'], defaults=ri_template)
+        # )
+
+        cities_list = each_row['cities'].split(',') if not check_value_is_nan(each_row['cities']) else []
+        prod_no_list = each_row['prod_no'].split(',') if not check_value_is_nan(each_row['prod_no']) else []
+        rest_no_list = each_row['rest (음식점)'].split(',') if not check_value_is_nan(each_row['rest (음식점)']) else []
+
+        
